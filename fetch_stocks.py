15ee23a700a5bf7c67626c6ea43e5fb6b82c74f5
1145811215@qq.com <1145811215@qq.com>
2020-05-28 10:32:43 +0800

    删除多余文件

--- fetch_stocks.py
+++ fetch_stocks.py
@@ -6,6 +6,7 @@ import json
 
 # 获取所有沪深A股
 def fetchAllStocks():
+    print '获取沪深全量股票...'
     url = "https://nufm1.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=C._A&sty=MPNSBAS&token=4f1862fc3b5e77c150a2b985b12db0fd"
     response = requests.get(url)
     with open('stocks.json', 'w') as f:
